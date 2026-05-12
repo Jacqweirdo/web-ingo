dcb9f4a0bb2037dbd7c0a323fb7ec793f0e7a88f
DiegoMiguel621 <130955808+DiegoMiguel621@users.noreply.github.com>
2026-05-12 15:45:05 -0600

    codigo de autorizacion a correo

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,13 +8,107 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
   correo: string = '';
   codigo: string = '';
   mostrarCodigo: boolean = false;
 
+  mensaje: string = '';
+  tipoMensaje: 'success' | 'error' | '' = '';
+
+  constructor(
+  private http: HttpClient,
+  private router: Router
+) {}
+
+  enviarCodigo() {
+
+    this.mensaje = '';
+    this.tipoMensaje = '';
+
+    if (!this.correo.trim()) {
+
+      this.mensaje = 'Ingresa un correo electrónico.';
+      this.tipoMensaje = 'error';
+
+      return;
+    }
+
+    this.http.post<any>(
+      'http://localhost:3000/api/verificar-correo',
+      {
+        correo: this.correo
+      }
+    ).subscribe({
+
+      next: (resp) => {
+
+        console.log(resp);
+
+        this.mostrarCodigo = true;
+
+        this.mensaje = 'Código generado correctamente.';
+        this.tipoMensaje = 'success';
+
+      },
+
+      error: (err) => {
+
+        console.error(err);
+
+        this.mensaje = 'Este correo no está autorizado.';
+        this.tipoMensaje = 'error';
+
+      }
+
+    });
+
+    
+
+  }
+
   reenviarCodigo() {
-  console.log('Código reenviado');
+    this.enviarCodigo();
+  }
+
+  validarCodigo() {
+
+  this.mensaje = '';
+  this.tipoMensaje = '';
+
+  if (!this.codigo.trim()) {
+    this.mensaje = 'Ingresa el código de verificación.';
+    this.tipoMensaje = 'error';
+    return;
+  }
+
+  if (this.codigo.trim().length !== 6) {
+    this.mensaje = 'El código debe tener 6 dígitos.';
+    this.tipoMensaje = 'error';
+    return;
+  }
+
+  this.http.post<any>(
+    'http://localhost:3000/api/validar-codigo',
+    {
+      correo: this.correo,
+      codigo: this.codigo
+    }
+  ).subscribe({
+    next: (resp) => {
+      this.mensaje = 'Acceso autorizado. Redirigiendo...';
+      this.tipoMensaje = 'success';
+
+      setTimeout(() => {
+        this.router.navigate(['/historial']);
+      }, 700);
+    },
+
+    error: (err) => {
+      this.mensaje = err.error?.mensaje || 'Código incorrecto.';
+      this.tipoMensaje = 'error';
+    }
+  });
 }
 
 }
-
